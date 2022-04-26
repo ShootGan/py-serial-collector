ab9c027765157a8acb7af4f3fc234315d9b5abf8
ShootGan <nawelpalepka@gmail.com>
2022-04-26 22:48:49 +0200

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+setup(
+    name="py-serial-collector",
+    version="0.0.1",
+    desciption = "Python based serial monitor with export to excel, csv,xml and more",
+    author="Pawel Nalepka",
+    author_email="nawelpalepka@gmail.com",
+    url ="https://github.com/ShootGan/serial_data_collector",
+    packages = find_packages()
+
+
+)
